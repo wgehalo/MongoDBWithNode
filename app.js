a07aebaebd76ab103939f3f44ea517dbc90e2656
wgehalo <wgehalo@gmail.com>
2017-10-23 21:51:31 -0400

    Uncommented code.

--- app.js
+++ app.js
@@ -9,16 +9,16 @@ var express = require('express'),
 app.engine('html', engines.nunjucks);
 app.set('view engine', 'html');
 app.set('views', __dirname + '/views');
-//app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, '/public')));
 
 // Handler for internal server errors
-/* function errorHandler(err, req, res, next) {
+function errorHandler(err, req, res, next) {
     console.error(err.message);
     console.error(err.stack);
     res.status(500).render('error_template', { error: err });
 }
- */
+
 MongoClient.connect('mongodb://localhost:27017/video', function(err, db) {
 
     assert.equal(null, err);
@@ -46,7 +46,7 @@ MongoClient.connect('mongodb://localhost:27017/video', function(err, db) {
         }
     });
     
-    // app.use(errorHandler);
+    app.use(errorHandler);
     
     var server = app.listen(3000, function() {
         var port = server.address().port;
